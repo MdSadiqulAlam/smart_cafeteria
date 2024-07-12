05b16362a739d6508aa41d00393f2f5d8ad12483
MdSadiqulAlam <sadiqul.fw@gmail.com>
2024-07-12 18:36:08 +0600

    order list completed all 4 functionalities done for use side

--- lib/pages/order_detail/components/order_detail_component.dart
+++ lib/pages/order_detail/components/order_detail_component.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
-
 import '../../../config/get_config.dart';
 
 class MyOrderDetailContainer extends StatelessWidget {
